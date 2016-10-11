c46c48e23bca3096ea55ce5ee5d27f362161e1a4
David Makogon <dmakogon@gmail.com>
2016-10-11 17:36:27 -0400

    updates for hrnug

---
+++ demos/net/UpdateActor/UpdateActor/Program.cs
@@ -0,0 +1,40 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateActor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            
+            var authToken = AuthTokens.Basic(appSettings["Username"], appSettings["Password"]);
+            using (var driver = GraphDatabase.Driver("bolt://"
+                + appSettings["Server"], authToken)
+
+             )
+            using (var session = driver.Session())
+            {
+                var actorName = "Rob Lowe";
+                var actorAge = 99;
+                var result = session.Run("MATCH (a:Actor) where a.name = {actorname} set a.age = {actorage} return a.name,a.age",
+                    new Dictionary<string, object> { { "actorname", actorName }, { "actorage", actorAge } });
+
+                foreach (var record in result)
+                {
+                    var list = record.Keys.Select(key => $"{key}: {record[key]}").ToList();
+                    Console.WriteLine(string.Join(", ", list));
+
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ demos/net/queryactor/queryactor/Program.cs
@@ -0,0 +1,37 @@
+﻿using Neo4j.Driver.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration; 
+namespace queryactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            var authToken = AuthTokens.Basic(appSettings["Username"], appSettings["Password"]);
+            using (var driver = GraphDatabase.Driver("bolt://"
+                + appSettings["Server"], authToken)
+
+             )
+            using (var session = driver.Session())
+            {
+                var myTitle = "The Matrix";
+                var result = session.Run("MATCH (a:Actor)-[:ACTS_IN]->(m:Movie) where m.title = {title} return a.name  limit 15",
+                    new Dictionary<string, object> { { "title", myTitle } });
+
+                    foreach (var record in result)
+                    {
+                        var list = record.Keys.Select(key => $"{key}: {record[key]}").ToList();
+                        Console.WriteLine(string.Join(", ", list));
+                        
+            
+                    }
+                Console.ReadLine();
+            }
+        }
+    }
+}
